634b09d9bd0d79cd2b3ae7589366f40683f73998
dimasciput <dimas.ciputra@gmail.com>
2023-05-11 12:40:05 +0700

    Add remember me in login screen

--- app/utils/storage.ts
+++ app/utils/storage.ts
@@ -1,4 +1,5 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import EncryptedStorage from "react-native-encrypted-storage";
 
 /**
  * Loads a string from storage.
@@ -55,6 +56,54 @@ export async function save(key: string, value: any): Promise<boolean> {
     return true;
   } catch {
     return false;
+  }
+}
+
+/**
+ * This asynchronous function securely saves a value under a specified key in encrypted storage.
+ *
+ * @async
+ * @export
+ * @param {string} key - The key under which the value will be stored.
+ * @param {any} value - The value to be stored.
+ * @returns {Promise<boolean>} - A promise that resolves to `true` if the value was saved successfully, and `false` otherwise.
+ * @throws Will throw an error if the saving process fails.
+ * @example
+ *
+ * saveEncrypted('username', 'JohnDoe')
+ *   .then(success => console.log(success))
+ *   .catch(e => console.error(e));
+ *
+ */
+export async function saveEncrypted(key: string, value: any) {
+  try {
+    await EncryptedStorage.setItem(key, value);
+    return true;
+  } catch {
+    return false;
+  }
+}
+
+/**
+ * This asynchronous function loads a value from encrypted storage given a specified key.
+ *
+ * @async
+ * @export
+ * @param {string} key - The key of the stored value to load.
+ * @returns {Promise<any | null>} - A promise that resolves to the stored value (parsed from JSON) if the load operation was successful, and `null` otherwise.
+ * @throws Will throw an error if the loading process fails, but this error is caught and results in a returned `null`.
+ * @example
+ *
+ * loadEncrypted('username')
+ *   .then(value => console.log(value))
+ *   .catch(e => console.error(e));
+ *
+ */
+export async function loadEncrypted(key: string): Promise<any | null> {
+  try {
+    return await EncryptedStorage.getItem(key);
+  } catch {
+    return null;
   }
 }
 
