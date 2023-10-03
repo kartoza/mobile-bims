bad6ed87c665aea2efcd89fb2efa825b718c07ee
Dimas <dimas.ciputra@gmail.com>
2023-10-03 13:19:33 +0700

    Can add wetland site without wetland data

--- app/screens/form-screen/site-form-screen.tsx
+++ app/screens/form-screen/site-form-screen.tsx
@@ -102,15 +102,11 @@ export const SiteFormScreen: React.FunctionComponent<
     if (response.data) {
       return response.data;
     } else {
-      Alert.alert(
-        'Error',
-        'Wetland data not found.\n' + 'Please use a different location.',
-        [
-          {
-            text: 'OK',
-          },
-        ],
-      );
+      Alert.alert('Error', 'Wetland data not found.', [
+        {
+          text: 'OK',
+        },
+      ]);
       return {};
     }
   };
@@ -123,20 +119,6 @@ export const SiteFormScreen: React.FunctionComponent<
         },
       ]);
       return;
-    }
-    if (updatedSiteData.ecosystemType === 'wetland') {
-      if (Object.keys(updatedSiteData.wetlandData).length === 0) {
-        Alert.alert(
-          'Error',
-          'Wetland site cannot be located outside of the wetland area.',
-          [
-            {
-              text: 'OK',
-            },
-          ],
-        );
-        return;
-      }
     }
     updatedSiteData.newData = false;
     updatedSiteData.synced = false;
